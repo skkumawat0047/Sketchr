7b5152beec19e93ce4aaebdb8a23dc0ccdb1f101
student skkumawat0047 <sk.kumawat0047@gmail.com>
2026-07-02 18:09:00 +0530

    konva size zoom from 50 to 500% added

--- src/components/Konva/Konva.jsx
+++ src/components/Konva/Konva.jsx
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 import { Circle, Image, Layer, Stage } from "react-konva";
 
 const Konva = () => {
-  const [zoom, setZoom] = useState(1);
+  const [zoom, setZoom] = useState(1.5);
 
   return (
     <>
       <div className="Konva bg-red-100 w-full h-screen flex justify-center items-center">
-        <div className="box w-[71%] h-[70%] flex justify-center items-center overflow-auto">
-          <Stage width={1200} height={700} scaleX={zoom} scaleY={zoom} className="bg-white" >
+        <div className="box w-[71%] h-[70%] flex justify-center items-center overflow-hidden">
+          <Stage width={1000} height={700} scaleX={zoom} scaleY={zoom} className="bg-white" >
             <Layer>
               <Circle
                 x={100}
@@ -26,7 +26,7 @@ const Konva = () => {
       <ul className="fixed right-5 bottom-2 z-50 px-5 py-2 rounded-full bg-gray-200 flex items-center gap-3">
         <li
           className="bg-white rounded-md px-2 py-1 cursor-pointer active:bg-slate-400"
-          onClick={() => setZoom((prev) => Math.max(0.2, prev - 0.1))}
+          onClick={() => setZoom((prev) => Math.max(0.5, prev - 0.1))}
         >
           <i className="fa-solid fa-minus"></i>
         </li>
