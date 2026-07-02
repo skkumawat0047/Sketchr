d475887202cc0a2ec25b269515580bdd24187c22
student skkumawat0047 <sk.kumawat0047@gmail.com>
2026-07-02 11:52:39 +0530

    custom table added

--- src/components/ShapeColorPen/Table.jsx
+++ src/components/ShapeColorPen/Table.jsx
@@ -6,7 +6,9 @@ const Table = () => {
       <ul className='w-fit px-5 rounded-full bg-gray-300 py-1 flex justify-center gap-3 text-2xl hover:[&>*]:cursor-pointer active:[&>li]:bg-blue-600 [&>*]:rounded-md'>
         <li><i class="fa-solid fa-table"></i></li>
         <li><i class="fa-solid fa-table-list"></i></li>
-        <span className='bg-red-200 px-1 rounded-md'>custom</span>
+        <span className='bg-yellow-50 rounded-md [&>*]:rounded-md flex justify-center'>
+          <input className='w-9 text-[15px] pl-1' type="number" min={0} max={20} placeholder='2' /><span>*</span>
+          <input className='w-9 text-[15px] pl-1' type="number" min={0} max={20} placeholder='3' /></span>
       </ul>
     </div>
   )
