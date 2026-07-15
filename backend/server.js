52384e4062b4db268326e07ec3776fc5c45c5fbb
student skkumawat0047 <sk.kumawat0047@gmail.com>
2026-07-15 15:58:56 +0530

    database barnch create for mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,80 +1,13 @@
-const mongoose = require("mongoose");
+const express = require('express');
+const cors = require('cors')
 
-mongoose.connect("mongodb://127.0.0.1:27017/Sketchr")
-  .then(() => {
-    console.log("MongoDB Connected Successfully");
-  })
-  .catch((err) => {
-    console.log("MongoDB Connection Error:", err);
-  });
+const app = express();
+const port = 3000;
 
-const Schema = mongoose.Schema;
-const ObjectId = Schema.ObjectId;
-console.log("Schema:", Schema,"objectid",ObjectId)
+app.use(cors());
+app.get('/',(req,res)=>{
+    res.send("Hello user!");
+    res.end();
+})
 
-const CanvasSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        default: "Untitled Canvas"
-    },
-    owner: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true
-    },
-    elements: [
-        {
-            id: {
-                type: String,
-                required: true
-            },
-            
-            type: {
-                type: String,
-                required: true,
-                enum: [
-                    "line",
-                    "rect",
-                    "circle",
-                    "ellipse",
-                    "arrow",
-                    "text",
-                    "table"
-                ]
-            },
-            x: {
-                type: Number,
-                default: 0
-            },
-            y: {
-                type: Number,
-                default: 0
-            },
-            zIndex: {
-                type: Number,
-                default: 0
-            },
-            rotation: {
-                type: Number,
-                default: 0
-            },
-            visible: {
-                type: Boolean,
-                default: true
-            },
-            locked: {
-                type: Boolean,
-                default: false
-            },
-            element: {
-                type: mongoose.Schema.Types.Mixed,
-                default: {}
-            }
-        }
-    ]
-},
-{
-    timestamps: true
-});
-
-module.exports = mongoose.model("Canvas", CanvasSchema);
+app.listen(port)
